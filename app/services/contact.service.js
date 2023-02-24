93b60bedde55d6e0c390ed8f6c2e10efd2136ba7
ttktrinh <tranthikieutrinh2018@gmail.com>
2023-02-24 13:43:00 +0700

    Cai dat cac handler truy xuat CSDL

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -29,6 +29,52 @@ class ContactService {
         return result.value;
     }
 
+    async find(filter) {
+        const cursor = await this.Contact.find(filter);
+        return await cursor.toArray();
+    }
+
+    async findByName(name) {
+        return await this.find({
+            name: { $regex: new RegExp(name), $options: "i" },
+        });
+    }
+
+    async findById(id) {
+        return await this.Contact.findOne({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        });
+    }
+
+    async update(id, payload) {
+        const filter = {
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        };
+        const update = this.extractConactData(payload);
+        const result = await this.Contact.findOneAndUpdate(
+            filter,
+            { $set: update },
+            { returnDocument: "after" }
+        );
+        return result.value;
+    }
+
+    async delete(id) {
+        const result = await this.Contact.findOneAndDelete({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        });
+        return result.value;
+    }
+
+    async findFavorite() {
+        return await this.find({ favorite: true });
+    }
+
+    async deleteAll() {
+        const result = await this.Contact.deleteMany({});
+        return result.deletedCount;
+    }
+
 }
 module.exports = ContactService;
 
